12d3976809239d8fa83398f9af37eb46aa27ba7a
Jadeeeeeeeeeee <105251276+Jadeeeeeeeeeee@users.noreply.github.com>
2024-03-03 22:48:04 +0000

    added two buttons and an error message in the gui

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ class Converter:
 
    def __init__(self):
 
+    #common format for all buttons
+    #Arial size 14 bold, with white text
+    button_font = ("Arial", "12", "bold")
+    button_fg = "#FFFFFF"
+
     # Set up the GUI Frame
     self.temp_frame = Frame(padx=10, pady=10)
     self.temp_frame.grid()
@@ -19,8 +24,35 @@ class Converter:
     self.temp_instructions = Label(self.temp_frame, text=instructions, wraplength=250, width=40, justify="left" )
 
     self.temp_instructions.grid(row=1)
-    
-    
+
+    self.temp_entry = Entry(self.temp_frame,
+                           font=("Arial", "14"))
+
+    self.temp_entry.grid(row=2, padx=10, pady=10)
+
+    error = "Please enter a number"
+    self.temp_error = Label(self.temp_frame, text=error,
+                           fg="#9C0000")
+    self.temp_error.grid(row=3)
+
+    #Conversion, help and history / export buttons
+    self.button_frame = Frame(self.temp_frame)
+    self.button_frame.grid(row=4)
+
+    self.to_celcius_button = Button(self.button_frame,
+                                   text="To Celcius",
+                                   bg="#990099",
+                                   fg=button_fg,
+                                   font=button_font)
+    self.to_celcius_button.grid(row=0, column=0)
+
+    self.to_farenheit_button = Button(self.button_frame,
+                                   text="To Farenheit",
+                                   bg="#009900",
+                                   fg=button_fg,
+                                   font=button_font)
+    self.to_farenheit_button.grid(row=0, column=1)
+        
 
 
 if __name__ == "__main__":
