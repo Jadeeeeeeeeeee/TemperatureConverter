8cf2902b9786d3afe160e4a45271a1cc4e3e08b4
Jadeeeeeeeeeee <105251276+Jadeeeeeeeeeee@users.noreply.github.com>
2024-03-04 23:18:32 +0000

    finished adding all the buttons, and made all the width of the button to be the same

--- main.py
+++ main.py
@@ -42,24 +42,33 @@ class Converter:
     self.to_celcius_button = Button(self.button_frame,
                                    text="To Celcius",
                                    bg="#990099",
-                                   fg=button_fg,
+                                   fg=button_fg, width=12,
                                    font=button_font)
-    self.to_celcius_button.grid(row=0, column=0)
+    self.to_celcius_button.grid(row=0, column=0, padx=5, pady=5)
 
     self.to_farenheit_button = Button(self.button_frame,
                                    text="To Farenheit",
                                    bg="#009900",
-                                   fg=button_fg,
+                                   fg=button_fg, width=12,
                                    font=button_font)
-    self.to_farenheit_button.grid(row=0, column=1)
+    self.to_farenheit_button.grid(row=0, column=1, padx=5, pady=5)
     
     self.to_help_button = Button(self.button_frame,
                                    text="Help / Info",
                                    bg="#CC6600",
-                                   fg=button_fg,
+                                   fg=button_fg, width=12,
                                    font=button_font)
-    self.to_help_button.grid(row=1, column=0)
+    self.to_help_button.grid(row=1, column=0, padx=5, pady=5)
 
+    self.to_farenheit_button.grid(row=0, column=1)
+    
+    self.to_history_button = Button(self.button_frame,
+                                   text="Help / Export",
+                                   bg="#004C99",
+                                   fg=button_fg,
+                                   font=button_font, width=12,
+                                   state=DISABLED)
+    self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
         
 
 
