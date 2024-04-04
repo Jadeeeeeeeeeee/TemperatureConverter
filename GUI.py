44b440996bd41979282e5d07580e875e2daf4078
Jadeeeeeeeeeee <105251276+Jadeeeeeeeeeee@users.noreply.github.com>
2024-04-04 20:51:44 +0000

    Finished making the history / export button it can now display what id does, the function is still about to be done

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from functools import partial # To prevent unwanted windows
 
 class Converter:
 
@@ -156,7 +157,7 @@ class Converter:
                                    text="Help / Info",
                                    bg="#CC6600",
                                    fg=button_fg, width=12,
-                                   font=button_font)
+                                   font=button_font, command=self.to_help)
     self.to_help_button.grid(row=1, column=0, padx=5, pady=5)
 
     self.to_farenheit_button.grid(row=0, column=1)
@@ -168,6 +169,65 @@ class Converter:
                                    font=button_font, width=12,
                                    state=DISABLED)
     self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
+
+  def to_help(self):
+    DisplayHelp(self)
+
+class DisplayHelp:
+
+
+  def __init__(self, partner):
+    #setup dialogue box and backround colour
+    backround = "#FFE6CC"
+
+    self.help_box = Toplevel()
+
+    #disable help button
+    partner.to_help_button.config(state=DISABLED)
+
+    #If users press cross at top, closses help and
+    #'releases' help button
+    self.help_box.protocol('WM_DELETE_WINDOW',
+                           partial(self.close_help, partner))
+
+    self.help_frame = Frame(self.help_box, width=300, height=200,
+                           bg=backround)
+    self.help_frame.grid()
+
+    self.help_heading_label = Label(self.help_frame, bg=backround,
+                                   text="Help / Info",font="Arial 14 bold")
+
+    self.help_heading_label.grid(row=0)
+
+    help_text = "To use the program, simply enter the temperature " \
+    "you wish to convert and then choose convert " \
+    "to either degrees Celcius(centigrade) or " \
+    "Fahrenheit.. \n\n" \
+    "Note that -273 degrees C " \
+    "(-459) is absulute zero (the coldest possible " \
+    "temperature). If you try to convert a " \
+    "temperature that is less than -273 degrees C, " \
+    "you will get an error message. \n\n" \
+    "To see your" \
+    "calculation history and export it into a text " \
+    "file, please click the 'History / Export' button"
+
+    self.help_text_label = Label(self.help_frame, bg=backround,
+                                text=help_text, wraplength=350, justify="left")
+
+    self.help_text_label.grid(row=1, padx=10)
+
+    self.dismiss_button = Button(self.help_frame, font=("Arial 12 bold"), 
+                                text="Dismiss", bg="#CC6000", fg="#FFFFFF",
+                                command=partial(self.close_help, partner)) 
+    self.dismiss_button.grid(row=2, padx=10, pady=10)
+
+  def close_help(self, partner):
+    #Put help button back to normal...
+    partner.to_help_button.config(state=NORMAL)
+    self.help_box.destroy()
+
+
 
 
 if __name__ == "__main__":  
