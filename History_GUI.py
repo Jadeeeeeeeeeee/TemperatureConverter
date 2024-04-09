4bb25a6317220d04718bfcad335bfddfb49daf7d
Jadeeeeeeeeeee <105251276+Jadeeeeeeeeeee@users.noreply.github.com>
2024-04-09 00:31:36 +0000

    made a function for history that shows the recent calculations in the range of the maximum recent calculations

--- History_GUI.py
+++ History_GUI.py
@@ -19,12 +19,12 @@ class Converter:
     self.button_frame.grid(row=0)
 
     self.to_history_button = Button(self.button_frame,
-                                   text="Help / Info",
-                                   bg="#CC6600",
+                                   text="History / Export",
+                                   bg="#004C99",
                                    fg=button_fg, width=12,
                                    state=DISABLED,
                                    font=button_font, command=self.to_history)
-    self.to_history_button.grid(row=1, column=0, padx=5, pady=5)
+    self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
 
     self.to_history_button.config(state=NORMAL)
 
@@ -50,19 +50,20 @@ class HistoryExport:
     self.history_frame = Frame(self.history_box, width=300, height=200)
     self.history_frame.grid()
 
-    self.history_heading_label = Label(self.history_frame, text="History / Export", font="Arial 14 bold")
+    self.history_heading_label = Label(self.history_frame, text="History / Export", font="Arial 16 bold")
 
     self.history_heading_label.grid(row=0)
 
+    #History text and label
     hist_text = "Below are your recent calculations - " \
                "showing  3 / 3 calculations. " \
                "All calculations are shown to the nearest degree"
 
-    self.text_instructions_label = Label(self.history_frame, text=hist_text, wraplength=300, justify="left", padx=10, pady=10)
+    self.text_instructions_label = Label(self.history_frame, text=hist_text, width=45, wraplength=300, justify="left", padx=10, pady=10)
 
     self.text_instructions_label.grid(row=1)
 
-    self.all_calcs_label = Label(self.history_frame, text="calculations go here", width=40, justify="left")
+    self.all_calcs_label = Label(self.history_frame, text="calculations go here", padx=10, pady=10, bg="#ffe6cc", width=40, justify="left")
 
     self.all_calcs_label.grid(row=2)
 
@@ -77,13 +78,25 @@ class HistoryExport:
     self.save_instructions_label.grid(row=3)
 
     self.filename_entry = Entry(self.history_frame, font=("Arial 14"), bg="#FFFFFF", width=25)
+    self.filename_entry.grid(row=4, padx=10, pady=10)
 
-    
-  
+    self.filename_error_label= Label(self.history_frame, text="Filename error goes here", fg="#9C0000", font=("Arial 12 bold"))
+    self.filename_error_label.grid(row=5)
+
+    self.button_frame = Frame(self.history_frame)
+    self.button_frame.grid(row=6)
+
+    self.export_button = Button(self.button_frame, font=("Arial 12 "), text="Export", bg="#004C99", fg="#FFFFFF", width=12)
+    self.export_button.grid(row=0, column=0, padx=10, pady=10)
+
+    self.dismiss_button = Button(self.button_frame, font=("Arial 12 bold"),text="Dismiss", bg="#666666", fg="#FFFFFF", width=12, command=partial(self.close_history, partner))
+
+    self.dismiss_button.grid(row=0, column=1, padx=10, pady=10)
+
 
   def close_history(self, partner):
     #Put help button back to normal...
-    partner.to_help_button.config(state=NORMAL)
+    partner.to_history_button.config(state=NORMAL)
     self.history_box.destroy()
 
 
